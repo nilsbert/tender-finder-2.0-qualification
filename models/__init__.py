6c33900da3f85466d32b3e0d2c5cafae0cb82e89
Nils Hyoma <nilsbert@gmail.com>
2026-04-23 01:15:50 +0200

    chore: design and sync updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from rating.models import Keyword, KeywordCreate, TenderACL
+from rating.models import Keyword, KeywordCreate, TenderACL, KeywordYamlModel, KeywordImportResult, KeywordImportSummary
 # Add any other missing imports here if found
